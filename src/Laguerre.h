a5ff84aa84fc11faefd72d365abaadec5b541839
Lenin <ttvleninn@gmail.com>
2017-03-28 16:52:26 -0600

    Final laguerre

--- src/Laguerre.h
+++ src/Laguerre.h
@@ -23,41 +23,118 @@ public:
 	Laguerre(){};
 	int calc_Laguerre();
 	virtual ~Laguerre(){};
-/*
+
 	template<class T>
 	void print_pol(polynomial<T> pol){
 		for(int i=0;i<=pol.degree();i++){
 			cout<<"+ "<<pol[i]<<"x^"<<i;
 		}
 		cout<<"\n";
-	}*/
+	}
+template <class T>
+	polynomial<T> derive( polynomial<T> poly){
+		/*polynomial<T> fd = poly;
+		double n = poly.degree() +1;
+		T comp (0,0);
+		for(double i = 0; i < n; i++){// calcula la primera derivada
+			if(i == 1){
+				fd[0] = poly[1];
+			}
+			else if(i == n-1){
+				fd[i-1] = poly[i]*i;
+				fd[n-1] = comp;
+			}
+			else
+				fd[i-1] = poly[i]*i;
 
+		}
+		return fd;*/
 
-	template <class T>
-	int calc_Laguerre(polynomial<T>  poly) {
+		polynomial<T> fd = poly;
+		double n = poly.degree();
+		T comp (0,0);
+		for(double i = 0; i < n; i++){// calcula la primera derivada
+			if(i == 0){
+				fd[0] = poly[1];
+			}
+			else if(i+1 == n){
+				fd[i] = poly[i+1]*(i+1);
+				fd[i+1] = comp;
+			}
+			else
+				fd[i] = poly[i+1]*(i+1);
+
+		}
+		cout<< "PREE DERIVE: "<<endl;
+		print_pol(fd);
+		return fd;
+
+	}
+
+	template<class T>
+	T zero (polynomial<T>  poly,polynomial<T>  fd,polynomial<T>  sd){
 		double m = poly.degree();
-		double n = m+1;
-		Pol *pol = new Pol();
-		int cont = m;
-		polynomial<T>  fd;
-		polynomial<T>  sd;
 		T x;
 		bool flag ;
 		T G;
 		T H;
 		T a1;
 		T a2;
+		T comp (0,0);
+	 x=comp;
+	 flag = true;
 
+	 while(flag){
+		 G = fd.evaluate(x)/poly.evaluate(x);
+		 H = pow(G,2)-((sd.evaluate(x)/poly.evaluate(x)));
+		 a1 = m / (G + sqrt((m-1)*(m*H-pow(G,2))));
+		 a2 = m / (G - sqrt((m-1)*(m*H-pow(G,2))));
+		 //cout<<"a1 : "<<a1<<endl;
+		 //cout<<"a2 : "<<a2<<endl;
+		 if (norm(a1) == norm(a1) && norm(a2) == norm(a2)){
+			 if (min(norm(a1),norm(a2)) < 0.000000001 )
+				 flag = false;
+			 else if(norm(a1)>norm(a2))
+				 x -= a2;
+			 else
+				 x -= a1;
+		 }
+		 else{
+			 flag = false;
+		 }
+	 }
+	 return x;
+
+	}
+
+	template <class T>
+	int calc_Laguerre(polynomial<T>  poly) {
+		//double m = poly.degree();
+		//double n = m+1;
+		Pol *pol = new Pol();
+		int cont = poly.degree();
+		polynomial<T> fd;
+		polynomial<T> sd;
+		T x;
+/*
+		T x;
+		bool flag ;
+		T G;
+		T H;
+		T a1;
+		T a2;
+*/
 		//for (int j = 0; j < m ;j++){
 		while (cont > 0){
-
+/*
 			fd= poly;
 			sd= poly;
 			complex<double> comp (0,0);
 			for(double i = 0; i < n; i++){// calcula la primera derivada
-				if(i == 1)
+				if(i == 1){
 					fd[0] = poly[1];
 
+					}
 				else if(i == n-1){
 					fd[i-1] = poly[i]*i;
 					fd[n-1] = comp;
@@ -68,18 +145,18 @@ public:
 			}
 			for(double i = 0; i < n; i++){//calcula la segunda derivada
 				if(i == 2){
-					sd[0] = fd[1];
+					//sd[0] = fd[1];
 				}
 				if(i == n-1){
 					sd[i-1] = fd[i]*i;
-					sd[n-1] = 0;
-					sd[n-2] = 0;
+					sd[n-1] = comp;
+					sd[n-2] = comp;
 				}
 				else
 					sd[i-1] = fd[i]*i;
 			}
-
-
+*/
+/*
 		 	x=comp;
 		 	flag = true;
 
@@ -101,15 +178,24 @@ public:
 		 		else{
 		 			flag = false;
 		 		}
-		 	}
-		 	cout<<"un cero es : "<<x<<endl;
-		 	polynomial<T> raiz{{-x,1.0}};
-		 	polynomial<T> residuo{{0.0,0.0, 0.0,0.0}};
-		 	poly = pol->divide(poly,raiz,residuo);
-		 	cont--;
+		 	}*/
+			fd = derive(poly);
+			sd = derive(fd);
+			//fd = poly;
+			//sd = fd;
+
+			x = zero(poly,fd,sd);
+		 cout<<"un cero es : "<<x<<endl;
+		 polynomial<T> raiz{{-x,1.0}};
+		 polynomial<T> residuo{{0.0,0.0, 0.0,0.0}};
+		 poly = pol->divide(poly,raiz,residuo);
+		 cont--;
+
 }
 	cout<<"Fin"<<endl;
-	exit (0);
+
+
+	//exit (EXIT_SUCCESS);
 	return 0;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 27, 2017
+ *      Author: fasm22
+ */
 
-#include "Muller.h"
+
+/****************************************************
+*     Program to demonstrate Mueller's method       *
+* ------------------------------------------------- *
+* Reference: BASIC Scientific Subroutines, Vol. II  *
+* By F.R. Ruckdeschel, BYTE/McGRAWW-HILL, 1981 [1]. *
+*                                                   *
+*                C++ version by J-P Moreau, Paris.  *
+*                       (www.jpmoreau.fr)           *
+* ------------------------------------------------- *
+* Example:  Find a real root of f(x)=(x+1)^5        *
+*                                                   *
+* Sample run:                                       *
+*                                                   *
+* Input the initial guess:                          *
+*    X0 = 0                                         *
+* What is the bond of this guess: 3                 *
+* Error criterion: 1e-6                             *
+* Maximum number of iterations: 100                 *
+*                                                   *
+* The calculated zero is X = -1.000476              *
+* The associated value is Y =  0.000000             *
+* The number of steps was: 47                       *
+*                                                   *
+****************************************************/
 #include "Laguerre.h"
+#include "Muller.h"
 #include <iostream>
 #include <boost/math/tools/polynomial.hpp>
 #include <complex>
@@ -11,26 +42,28 @@ using namespace boost::math::tools;
 
 
 int main() {
-	//cout << std::setprecision(10) << endl;
+	/*Pol* n = new Pol();
 
+	polynomial<double> const b{{-2.0, 1.0}};
 
-
+	n->print_pol(b);*/
 	//polynomial<double> const b{{4.0, 1.0,-2.0,3.0,2.0}};
   //polynomial<double> const b{{6.0,-5.0,1}};
 	//polynomial<complex<double>> const b{{36.0, -27.0,-13.0,3.0,1.0}};
 	//polynomial<complex<double>> b{{5.0, 1.0,1.0,1.0,1.0}};
 	//polynomial<complex<double>> b{{36, -36.0,13.0,-4.0,1.0}};
+	polynomial<complex<double>> b{{-8.0, -4.0,2.0,-1.0,1.0}};
 	//polynomial<complex<double>> b{{-60.0, -23.0,2.0, 1.0}};
-	//Laguerre *lag = new Laguerre();
-	//lag->calc_Laguerre(b);
+	polynomial<complex<double>> fd{{-23.0, 4.0,3.0, 0.0}};
+	polynomial<complex<double>> sd{{4.0, 6.0,0.0, 0.0}};
+	Laguerre *lag = new Laguerre();
+	lag->calc_Laguerre(b);
+	//cout << "b = " << formula_format(b) << "\n\n";
+/*
+	Muller* m = new Muller();
 
-	polynomial<complex<double>> b{{-60.0, -23.0,2.0, 1.0}};
-	cout<<"### Laguerre ###"<<endl;
-	Laguerre *g = new Laguerre();
-	g->calc_Laguerre(b);
-	cout<<"### #### ###"<<endl;
-	cout<<endl;
-
+	m->findZeros(1,3,0.00000000001,500);
+*/
 
 cout << "b = " <<endl;
   return 0;
