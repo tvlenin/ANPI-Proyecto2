56d8010565096b3353b28eb618ba17b5b634a21c
Fabian <fasm2296@gmail.com>
2017-03-28 21:10:26 -0600

    Se añade la clase TestFramework para la verificacion de tiempos de ejecución

---
+++ src/TestFramework.h
@@ -0,0 +1,23 @@
+/*
+ * TestFramework.h
+ *
+ *  Created on: Mar 28, 2017
+ *      Author: fasm22
+ */
+
+#ifndef SRC_TESTFRAMEWORK_H_
+#define SRC_TESTFRAMEWORK_H_
+
+class TestFramework{
+public:
+	TestFramework(){};
+	virtual ~TestFramework(){};
+
+	void testTime(){
+
+	}
+};
+
+
+
+#endif /* SRC_TESTFRAMEWORK_H_ */
